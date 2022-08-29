6dbc3fd4e1e1ca4f754d7adfd7f4321b18fa1fab
V.Tsutsurin <v.tsutsurin@yandex.ru>
2022-08-29 22:31:11 +0300

    ДЗ 11

--- ClassWork.py
+++ ClassWork.py
@@ -1,40 +1,7 @@
-import math
+import random
 
-print(
-    'Написать функцию, принимающую некоторую информацию о геометрической фигуре и рассчитывающую площадь данной фигуры')
-print()
-print("Тестовые значения:figure_type = 'rhombus', d1=10, d2=8 \nfigure_type='square', c=5"
-      "\nfigure_type='trapezoid', a=12, b=3, h=6 \nfigure_type='circle', r=18 \nfigure_type='unknown', a=1, b=2, c=3")
+lst = [random.randint(1, 40) for i in range(10)]
 
 
-def figure_area(figure_type=None, **kwargs):
-    if figure_type == 'rhombus':
-        d1 = kwargs.get('d1')
-        d2 = kwargs.get('d2')
-        s = (d1 * d2) / 2
-        return s
-    elif figure_type == 'square':
-        c = kwargs.get('c')
-        s = c ** 2
-        return s
-    elif figure_type == 'trapezoid':
-        a = kwargs.get('a')
-        b = kwargs.get('b')
-        h = kwargs.get('h')
-        s = (0.5 * ((a + b) * h))
-        return s
-    elif figure_type == 'circle':
-        r = kwargs.get('r')
-        p = math.pi
-        s = p * (r ** 2)
-        return s
-    else:
-        return 'invalid data'
-
-
-print()
-print(figure_area(figure_type='rhombus', d1=10, d2=8))
-print(figure_area(figure_type='square', c=5))
-print(figure_area(figure_type='trapezoid', a=12, b=3, h=6))
-print(figure_area(figure_type='circle', r=18))
-print(figure_area(figure_type='unknown', a=1, b=2, c=3))
+print(lst)
+print(list(filter(lambda b: 10 < b < 20, lst)))
---
+++ HomeWork/DZ 11.py
@@ -0,0 +1,49 @@
+# Задание 1
+print('Дано три словаря: {1:10, 2:20},{3:30, 4:40} и {5:50, 6:60}.Объедините данные словари в один')
+
+d1 = {1: 10, 2:20}
+d2 = {3: 30, 4: 40}
+d3 = {5: 50, 6: 60}
+print()
+
+d1.update(d2)
+d1.update(d3)
+print(d1)
+
+# # Задание 2
+print("Дан словарь {'emp1': {'name': 'Jhon', 'salary': 7500},'emp2': {'name': 'Emma', 'salary': 8000},\n"
+      "'emp3': {'name': 'Brad', 'salary': 6500}}.Измените значение зарплаты Бреда с 6500 на 8500")
+
+d = {'emp1': {'name': 'Jhon', 'salary': 7500}, 'emp2': {'name': 'Emma', 'salary': 8000},
+     'emp3': {'name': 'Brad', 'salary': 6500}}
+
+print()
+
+print(d['emp3'])
+d['emp3']['salary'] = 8500
+
+for i in d:
+    print(i)
+    for x in d[i]:
+        print(x, ":", d[i][x])
+
+# Задание 3
+print('пользователь вводит данные о количестве студентов, их фамилии, имена и балл для каждого.\n'
+      'Программа должна определить средний балл и вывести фамилии и имена студентов, чей балл выше среднего')
+print()
+
+students = {}
+student_value = int(input('Количество студентов: '))
+marks = 0
+for i in range(student_value):
+    number = input(str(i+1) + '-й студент: ')
+    mark = int(input('Балл: '))
+    students[number] = mark
+    marks += mark
+
+average = marks / student_value
+print()
+print(f'Средний балл: {average:.0f}. Студенты с баллом выше среднего:')
+for i in students:
+    if students[i] > average:
+        print(i)
