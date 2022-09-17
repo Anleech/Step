4a17bcc7b0dfbe82f1b68eddd6ff688407422462
V.Tsutsurin <v.tsutsurin@yandex.ru>
2022-09-17 22:07:50 +0300

    ДЗ 16

--- ClassWork.py
+++ ClassWork.py
@@ -1,11 +1,12 @@
 import re
 
-
-def validate_phone(name):
-    reg = r"^\+?7[ (]*\d+[ )]*[\d -]{8,10}"
-    return re.search(reg, name).group()
-
-print(validate_phone('+7 499 456-45-78'))
-print(validate_phone('+74994564578'))
-print(validate_phone('7 (499) 456-45-78'))
-print(validate_phone('7 (499) 456-45-78'))
+s = "Я ищу совпадения в 2021 года. И я из найду в 2 счёта"
+reg = r'([0-9]+)\s(\D+)'
+print(re.search(reg, s).group())
+# m = re.search(reg, s)
+# print(m[0])
+# print(m[1])
+# print(m[2])
+# print(re.findall(reg, s))
+print(re.search(reg, s).group(1))
+print(re.search(reg, s).group(2))
