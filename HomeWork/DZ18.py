6d8dfaded83dee335e3cce636b5c94eb27737ed7
V.Tsutsurin <v.tsutsurin@yandex.ru>
2022-09-24 23:41:21 +0300

    ДЗ 18.1

--- HomeWork/DZ18.py
+++ HomeWork/DZ18.py
@@ -12,24 +12,25 @@
 #
 # f = open("text.txt", "r")
 # read_f = [li.rstrip('\n') for li in f.readlines()]
+# f.close()
 # pos1 = int(input('pos1 = '))
 # pos2 = int(input('pos2 = '))
-#
 # if 0 <= pos1 <= len(read_f) and 0 <= pos2 <= len(read_f) and pos1 != pos2:
 #     read_f[pos1], read_f[pos2] = read_f[pos2], read_f[pos1]
 # else:
 #     print("Индекс введен неверно!")
-# f.close()
+#
 #
 # f = open("text.txt", "w")
 # for i in read_f:
 #     f.write(i + '\n')
 # f.close()
 #
-# my_file = open("text.txt", "r")
-# read_file = [li.rstrip('\n') for li in my_file.readlines()]
+# f = open("text.txt", "r")
+# read_file = [li.rstrip('\n') for li in f.readlines()]
 # for i in read_file:
 #     print(i)
+# f.close()
 
 # Задание 2
 # print('''Реверсирование строк файла (перестановка строк в обратном порядке)
@@ -44,6 +45,7 @@
 # f.close()
 #
 # f = open("text.txt", "r")
+# f.close()
 # read_f = [li.rstrip('\n') for li in f.readlines()]
 # read_f.reverse()
 #
@@ -52,26 +54,29 @@
 #     f.write(i + '\n')
 # f.close()
 #
-# my_file = open("text.txt", "r")
-# read_file = [li.rstrip('\n') for li in my_file.readlines()]
+# f = open("text.txt", "r")
+# read_file = [li.rstrip('\n') for li in f.readlines()]
 # for i in read_file:
 #     print(i)
+# f.close()
 
 
 # Задание 3
-
+#
 print('Объединение двух файлов')
 
-f = open("text1.txt", "w")
-f.write("Тааль.\nПресноводное озеро Филиппин.\nНаходится на острове Лусон.\nВ провинции Батангас.")
-f.close()
+f1 = open("text1.txt", "w")
+f1.write("Тааль.\nПресноводное озеро Филиппин.\nНаходится на острове Лусон.\nВ провинции Батангас.")
+f1.close()
 f2 = open("text2.txt", "w")
 f2.write("Озеро заполняет большую вулканическую кальдеру Тааль.\nОобразованную очень большим извержением.")
 f2.close()
 
 f1 = open("text1.txt", "r")
+f1.close()
 read_f1 = [li.rstrip('\n') for li in f1.readlines()]
 f2 = open("text2.txt", "r")
+f2.close()
 read_f2 = [li.rstrip('\n') for li in f2.readlines()]
 
 file3 = read_f1+read_f2
@@ -80,4 +85,4 @@ f3 = open("text3.txt", "w")
 for i in file3:
     f3.write(i + '\n')
 f3.close()
-
+#
