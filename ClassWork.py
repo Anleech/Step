59c69b5644d312e3706608d9fdbc04a20f7219e3
V.Tsutsurin <v.tsutsurin@yandex.ru>
2022-10-09 13:06:57 +0300

    ДЗ 23

--- ClassWork.py
+++ ClassWork.py
@@ -1,8 +1,53 @@
-def f(a, x=5, *args, **kwargs):
-    print(a)
-    print(x)
-    print(args)
-    print(kwargs)
+from abc import ABC, abstractmethod
 
 
-f(1, 2, 3, 4, b='test', c='hi')
+class Currency(ABC):
+    def __init__(self, value):
+        self.value = value
+
+    @abstractmethod
+    def covert_to_rub(self):
+        pass
+
+    def print_value(self):
+        print(self.value, end=' ')
+
+
+class Dollar(Currency):
+    rate_to_rub = 74.16
+    suffix = 'USD'
+
+    def covert_to_rub(self):
+        rub = self.value * Dollar.rate_to_rub
+        return rub
+
+    def print_value(self):
+        super().print_value()
+        print(Dollar.suffix, end=' ')
+
+
+class Euro(Currency):
+    rate_to_rub = 90.14
+    suffix = 'EUR'
+
+    def covert_to_rub(self):
+        rub = self.value * Euro.rate_to_rub
+        return rub
+
+    def print_value(self):
+        super().print_value()
+        print(Euro.suffix, end=' ')
+
+
+d = [Dollar(5), Dollar(10), Dollar(50), Dollar(100)]
+e = [Euro(5), Euro(10), Euro(50), Euro(100)]
+
+print('*' * 30)
+for elem in d:
+    elem.print_value()
+    print(f'= {elem.covert_to_rub():.2f} RUB')
+
+print('*' * 30)
+for elem in e:
+    elem.print_value()
+    print(f'= {elem.covert_to_rub():.2f} RUB')
