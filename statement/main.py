c3d1790c40a6c81d2f0df44a73e5ada030072d3a
V.Tsutsurin <v.tsutsurin@yandex.ru>
2022-11-08 20:01:39 +0300

    DZ_30.1

--- statement/main.py
+++ statement/main.py
@@ -1,10 +1,12 @@
 from statement import *
 
+print('Расчет заработной платы')
+print('='*50)
 a = admin.Admin(1, 'Валерий Задорожный')
 a.show_info()
-
+print()
 w = worker.Worker(2, 'Илья Кромин', 6)
 w.show_info()
-
+print()
 m = manager.Manager(3, 'Николай Хорольский', 25)
 m.show_info()
