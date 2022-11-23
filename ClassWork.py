d07f0add4bb11cd9c947e43281c1166fad0da5ab
V.Tsutsurin <v.tsutsurin@yandex.ru>
2022-11-23 20:22:17 +0300

    DZ34

--- ClassWork.py
+++ ClassWork.py
@@ -1,54 +1,117 @@
+# from bs4 import BeautifulSoup
+
+# f = open('index.html').read()
+# soup = BeautifulSoup(f, "html.parser")
+# row = soup.find("div", class_="name").get_text()
+# row = soup.findAll("div", class_="name")
+# row = soup.find_all("div", class_="row")[1].find("div", class_='links').text
+# row = soup.find("div", {"class": "name"})
+# row = soup.find("div", {"data_set": "salary"})
+# row = soup.find("div", text="Alena").find_parent(class_="row")
+# row = soup.find("div", id="whois3").find_next_sibling()
+# row = soup.find("div", id="whois3").find_previous_sibling()
+# print(row)
+
+
+# def get_copywriter(tag):
+#     whois = tag.find("div", class_="whois").text
+#     if "Copywriter" in whois:
+#         return tag
+#     return None
+#
+# f = open('index.html', encoding='utf-8').read()
+# soup = BeautifulSoup(f, "html.parser")
+# copywriter = []
+# row = soup.find_all('div', class_='row')
+# for i in row:
+#     cw = get_copywriter(i)
+#     if cw:
+#         copywriter.append(cw)
+# print(copywriter)
+
+# import re
+
+# def get_salary(s):
+#     pattern = r"\d+"
+#     res = re.findall(pattern, s)[0]
+#     print(res)
+#
+# f = open('index.html', encoding='utf-8').read()
+# soup = BeautifulSoup(f, "html.parser")
+# salary = soup.find_all("div", {"data-set": "salary"})
+# for i in salary:
+#     get_salary(i.text)
+
+from bs4 import BeautifulSoup
+import requests
+import re
 import csv
 
-# with open("data.csv") as r_file:
-#     file_reader = csv.reader(r_file, delimiter=";")
-#     count = 0
-#     for row in file_reader:
-#         # print(row)
-#         if count == 0:
-#             print(f"Файл содержит: {', '.join(row)}")
-#         else:
-#             print(f"    {row[0]} - {row[1]}. Родился в {row[2]} году")
-#         count += 1
-#     print(f"Всего в файле {count} строки")
-
-# with open("data.csv") as r_file:
-#     field_names = ['Имя',  'Профессия',  'Год рождения']
-#     file_reader = csv.DictReader(r_file, delimiter=";", fieldnames=field_names)
-#     count = 0
-#     for row in file_reader:
-#         if count == 0:
-#             print(f"Файл содержит: {', '.join(row)}")
-#         print(f"{row['Имя']} - {row['Профессия']}. Родился в {row['Год рождения']} году")
-#         count += 1
-
-# with open('student.csv', 'w') as f:
-#     writer = csv.writer(f, delimiter=';', lineterminator='\r')
-#     writer.writerow(["Имя", "Класс", "Возраст"])
-#     writer.writerow(["Женя", "9", "15"])
-#     writer.writerow(["Саша", "5", "12"])
-#     writer.writerow(["Маша", "11", "18"])
-
-
-# data = [['hostname', 'vendor', 'model', 'location'],
-#         ['sw1', 'Cisco', '3750', 'London, Best str'],
-#         ['sw2', 'Cisco', '3850', 'Liverpool, Better str'],
-#         ['sw3', 'Cisco', '3650', 'Liverpool, Better str'],
-#         ['sw4', 'Cisco', '3650', 'London, Best str']]
+# req = requests.get('https://ru.wordpress.org/')
 #
-# with open('data_new.csv', 'w') as f:
-#     writer = csv.writer(f, delimiter=',', lineterminator='\r', quoting=csv.QUOTE_NONNUMERIC)
-#     # for row in data:
-#     #     writer.writerow(row)
-#     writer.writerows(data)
-#
-# with open('data_new.csv') as f:
-#     print(f.read())
+# print(req.text)
 
-with open('student1.csv', 'w') as f:
-    names = ["Имя", "Возраст"]
-    file_writer = csv.DictWriter(f, delimiter=";", lineterminator='\r',fieldnames=names)
-    file_writer.writeheader()
-    file_writer.writerow(["Женя", "9", "15"])
-    file_writer.writerow(["Саша", "5", "12"])
-    file_writer.writerow(["Маша", "11", "18"]
+
+# def get_html(url):
+#     r = requests.get(url)
+#     return r.text
+#
+#
+# def get_data(html):
+#     soup = BeautifulSoup(html, "lxml")
+#     p1 = soup.find("header", id="masthead").find("p", class_="site-title")
+#     return p1
+#
+#
+# def main():
+#     url = "https://ru.wordpress.org/"
+#     print(get_data(get_html(url)))
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+def refind(s):
+    res = re.sub(r"\D+", "", s)
+    return res
+
+
+def write_csv(data):
+    with open("plugins.csv", "a") as f:
+        writer = csv.writer(f, lineterminator="\n", delimiter=";")
+        writer.writerow((data['name'], data['url'], data['rating']))
+
+
+def get_data(html):
+    soup = BeautifulSoup(html, "lxml")
+    p1 = soup.find_all('section', class_="plugin-section")[1]
+    plugins = p1.find_all('article')
+
+    for plugin in plugins:
+        name = plugin.find("h3").text
+        # url = plugin.find("h3").find("a")['href']
+        url = plugin.find("h3").find("a").get('href')
+        rating = plugin.find("span", class_="rating-count").find("a").text
+        r = refind(rating)
+
+        data = {'name': name, 'url': url, 'rating': r}
+
+        write_csv(data)
+
+    # return len(plugins)
+
+
+def main():
+    url = "https://ru.wordpress.org/plugins/"
+    get_data(get_html(url))
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ HomeWork/DZ34.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import csv
+
+
+def get_html(url):
+    r = requests.get(url)
+    return r.text
+
+
+
+def write_csv(data):
+    with open("best_recipes.csv", "a") as f:
+        writer = csv.writer(f, lineterminator="\n", delimiter=";")
+        writer.writerow((data['tittle'], data['time'], data['url']))
+
+
+def get_data(html):
+    soup = BeautifulSoup(html, "lxml")
+    recipes = soup.find_all('div', class_='rating-block ib')[3]
+    recipes_best = recipes.find_all('div', class_='cn-item')
+
+    for i in recipes_best:
+        title = i.find("a", class_='h5').text
+        time = i.find('div', class_='level-right').find('span').text
+        url = i.find('div', class_='photo is-relative').find('a').get('href')
+        u = 'https://1000.menu' + url
+
+        data = {'tittle': title, 'time': time, 'url': u}
+
+        write_csv(data)
+
+
+def main():
+    url = "https://1000.menu/"
+    get_data(get_html(url))
+
+
+if __name__ == '__main__':
+    main()
