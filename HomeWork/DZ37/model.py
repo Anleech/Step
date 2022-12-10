330acd63205c4d7e8820efa5ddea96cad70ac3e9
V.Tsutsurin <v.tsutsurin@yandex.ru>
2022-12-10 16:09:14 +0300

    DZ37

--- HomeWork/DZ37/model.py
+++ HomeWork/DZ37/model.py
@@ -3,23 +3,23 @@ import os.path
 
 
 class Article:
-    def __init__(self, title, ganr, producer, year, duration, studio, actors):
+    def __init__(self, title, genre, producer, year, duration, studio, actor):
         self.title = title
-        self.ganr = ganr
+        self.genre = genre
         self.producer = producer
         self.year = year
         self.duration = duration
         self.studio = studio
-        self.actors = actors
+        self.actor = actor
 
     def __str__(self):
-        return f"{self.title} - жанр ({self.ganr}) - режиссер ({self.producer})"
+        return f"{self.title} (Жанр - {self.genre}. {self.year} год)"
 
 
 class ArticleModel:
     def __init__(self):
-        self.db_name = 'move_catalog.txt'
-        self.articles = self.load_data()  # {}
+        self.db_film = 'film.txt'
+        self.articles = self.load_data()
 
     def add_article(self, dict_article):
         article = Article(*dict_article.values())
@@ -31,13 +31,13 @@ class ArticleModel:
     def get_singe_article(self, user_title):
         article = self.articles[user_title]
         dict_article = {
-            "название": article.title,
-            "жанр": article.ganr,
+            "название фильма": article.title,
+            "жанр": article.genre,
             "режиссер": article.producer,
             "год выпуска": article.year,
             "длительность": article.duration,
             "студия": article.studio,
-            "актеры": article.actors
+            "актеры": article.actor
         }
         return dict_article
 
@@ -45,12 +45,12 @@ class ArticleModel:
         return self.articles.pop(user_title)
 
     def load_data(self):
-        if os.path.exists(self.db_name):
-            with open(self.db_name, 'rb') as f:
+        if os.path.exists(self.db_film):
+            with open(self.db_film, 'rb') as f:
                 return pickle.load(f)
         else:
             return dict()
 
     def save_data(self):
-        with open(self.db_name, 'wb') as f:
+        with open(self.db_film, 'wb') as f:
             pickle.dump(self.articles, f)
