9fe79a22de3b65091158d1456749d01d2a7dbb75
V.Tsutsurin <v.tsutsurin@yandex.ru>
2023-01-03 08:38:19 +0300

    DZ43

---
+++ geometry/DZ 43.py
@@ -0,0 +1,15 @@
+import sqlite3 as sq
+
+data = [('Стол', 12), ('Стул', 15), ('Шкаф', 7), ('Тумба', 30), ('Ковёр', 58)]
+
+with sq.connect("goods.db") as con:
+    cur = con.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS goods (
+        goods_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        product_name TEXT,
+        amount INTEGER
+    )""")
+
+    cur.executemany("INSERT INTO goods VALUES(NULL,?,?)", data)
+    
