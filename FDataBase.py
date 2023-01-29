a05615a6f218d93f41c69ad5a689efec5851a648
V.Tsutsurin <v.tsutsurin@yandex.ru>
2023-01-29 21:15:51 +0300

    DZ47

--- FDataBase.py
+++ FDataBase.py
@@ -29,7 +29,8 @@ class FDataBase:
                 print("Статья с таким url уже существует")
                 return False
             base = url_for('static', filename='images')
-            text = re.sub(r'(?P<tag><img\s+[^>]*src=)(?P<quote>[\'"])(?P<url>.+?)(?P=quote)>', r'\g<tag>' +base +r"/\g<url>>", text)
+            text = re.sub(r'(?P<tag><img\s+[^>]*src=)(?P<quote>[\'"])(?P<url>.+?)(?P=quote)>', r'\g<tag>' + base +
+                          r"/\g<url>>", text)
             tm = math.floor(time.time())
             self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?, ?)", (title, text, url, tm))
             self.__db.commit()
