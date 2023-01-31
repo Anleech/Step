161ff2bd43c7f39f2f36e038b67f32e183ffef23
V.Tsutsurin <v.tsutsurin@yandex.ru>
2023-01-31 22:14:00 +0300

    DZ48

--- My Project/first.py
+++ My Project/first.py
@@ -1,6 +1,33 @@
-from flask import Flask, render_template, url_for
+import sqlite3
+import os
+from flask import Flask, render_template, url_for, request, flash, get_flashed_messages
+
+
+DATABASE = '/tmp/flsk.db'
+DEBUG = True
+SECRET_KEY = 'e9e701b7815f5dce89f37cd8ea3f7f2b19fffb75'
+
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsk.db')))
+
+
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
 
 menu = [
     {"name": "Главная страница", "url": "home"},
@@ -32,10 +59,19 @@ def blog():
     return render_template("blog.html", title="Блог", menu=menu)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
+    if request.method == "POST":
+        if len(request.form['message']) >= 10:
+            flash('Сообщение отправлено!', category='success')
+        else:
+            flash('Сообщение не отправлено.Минимальная длинна должна быть 10 символов', category='error')
     return render_template("contact.html", title="Контакты", menu=menu)
 
+
+@app.errorhandler(404)
+def page_nat_found(error):
+    return render_template("page404.html", title="Страница не найденна", menu=menu)
 
 if __name__ == "__main__":
     app.run(debug=True)
